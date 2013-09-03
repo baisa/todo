7ee523fe27d559c841b137773eb7d9b8712ac533
Barbara Slawinska <slawinska.barbara@gmail.com>
2013-09-03 10:51:42 +0200

    first commit with import

--- cli.rb
+++ cli.rb
@@ -1,5 +1,72 @@
 require_relative 'table'
 
+print "> "
 
-#array = [['Number', 'Name'], [1, 'Foo'], [2, 'Bar'], [3, 'Baz']]
-#print pretty_table(array)
+my_hash = {}
+
+puts 'Preload?'
+if gets.chomp == 'y'
+
+	my_hash = {"CIUCIK"=>{"Walk"=>["Slawosz", "Today", "No"], 
+		  "Play"=>["Slawosz", "Tonight", "No"]},
+		"Basia"=>
+		  {"Massage"=>["Slawosz", "Today", "No!"], "Give wine"=>["Slawosz", "Tonight", "No"]}}
+    name = 'CIUCIK'
+end
+print ">"
+while input = gets.chomp
+	print "> "
+	break if input == "END"
+	
+	if input == "ADD LIST"
+		print "Name of the list: "
+		name = gets.chomp
+		my_hash[name] = {}
+		print "List #{name} is created.\n"
+
+	elsif input == "ADD TASK"
+		print "Please name the task""\n""> "
+		task = gets.chomp
+		my_hash[name][task]
+		print "Please name a responsible person""\n"">"
+		person = gets.chomp
+		print "Give a deadline""\n"">"
+		deadline = gets.chomp
+		print "Check if done""\n"">"
+		status = gets.chomp
+		my_hash[name][task] = [person, deadline, status]
+
+	elsif input == "CURRENT LIST"
+		print "#{name}""\n"
+		print "> "
+		
+	elsif input == "CHANGE LIST"
+		print "Name a wanted list: "
+		name = gets.chomp
+		my_hash[name][task]
+		print "List is changed to #{name}.\n"
+		
+
+	elsif input == "INSPECT"
+		print my_hash"\n""> "
+		print "\n"
+
+	elsif input == "SHOW ALL TASKS"
+		all_tasks = my_hash[name].to_a
+		all_tasks.collect! { |elem| elem.flatten!}
+		print pretty_table(all_tasks)
+		print "\n"
+
+
+	elsif input == "SHOW ALL LISTS"
+		my_hash.each_key { |k| print "#{k} "}
+		print "\n"
+
+
+		
+	else
+		print "Incorrect phrase. Please write again.""\n""> "
+	end
+end
+
+p my_hash
